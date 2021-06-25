f1e16993a7e7a94fdcdebb7acf35742479cb3b16
Widky Nguyen <sba.nttri@gmail.com>
2021-06-25 16:20:17 +0700

    Design page service and single service - done

--- wp-content/themes/hotel-center-lite-child/inc/libs/owlcarousel/js/scripts.js
+++ wp-content/themes/hotel-center-lite-child/inc/libs/owlcarousel/js/scripts.js
@@ -84,4 +84,35 @@ jQuery(document).ready(function() {
             }
         }
     });
+
+    //other service
+    jQuery('#multi-carousel-service .owl-carousel').owlCarousel({
+        loop: false,
+        nav: true,
+        navText: ['<span class="prev-icon"></span>', '<span class="next-icon"></span>'],
+        dots: false,
+        margin: 35,
+
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2,
+                margin: 28,
+            },
+            1024: {
+                items: 2,
+                margin: 31,
+            },
+            1440: {
+                items: 3,
+                margin: 31
+            },
+            1920: {
+                items: 3,
+                margin: 31
+            }
+        }
+    });
 });
---
+++ wp-content/themes/hotel-center-lite-child/single-service.php
@@ -0,0 +1,216 @@
+<?php 
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package Hotel Center Lite
+ */
+
+get_header();
+
+include dirname( __FILE__ ) . '/inc/lang/translate.php';
+
+?>
+<style>
+<?php include dirname(__FILE__) . '/assets/css/page-service.css';
+?>
+</style>
+<?php
+$terms = wp_get_object_terms( get_the_ID(), 'service-category');
+// var_dump($terms);exit;
+$term_name = $terms[0]->name;
+$term_des = $terms[0]->description;
+?>
+<div id="sService">
+    <div class="container css">
+        <div class="spost">
+            <div class="sp-header">
+                <div class="sp-header-wrap">
+                    <h3 class="sp-cat-title text-center"><?php echo $term_name; ?></h3>
+                    <div class="sp-share">
+                        <a href="#">
+                            <i class="fas fa-share-alt"></i>
+                            <span><?php echo $strButtonShare; ?></span>
+                        </a>
+                    </div>
+                </div>
+                <p class="sp-excerpt"><?php echo get_the_excerpt(); ?></p>
+            </div>
+            <div class="sp-body">
+                <h3 class="sp-title"><?php the_title(); ?></h3>
+                <div class="sp-img">
+                    <?php
+                    $sliderCat = get_field('slide_thumbnail', get_the_ID());
+                    // echo "<pre>";var_dump($lengthArray);exit;
+                    if($sliderCat && $sliderCat['img_1'] != false) :
+                    $lengthArray = count($sliderCat);
+                    ?>
+                    <div class="post-img-slide-wrap">
+                        <div id="carouselImgSlideModal" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <?php if (has_post_thumbnail( get_the_ID() ) ): ?>
+                                <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'single-post-thumbnail' ); ?>
+                                <li data-target="#carouselImgSlideModal" data-slide-to="0" class="active"
+                                    style="background: url('<?php echo $image[0]; ?>')">
+                                </li>
+
+                                <?php endif; ?>
+
+                                <?php for($i = 0; $i < $lengthArray; $i++) : ?>
+
+                                <?php 
+                                $nameImg = 'img_'.($i + 1);
+                                if($sliderCat[$nameImg] != false) : ?>
+
+                                <li data-target="#carouselImgSlideModal"
+                                    data-slide-to="<?php if(!has_post_thumbnail( get_the_ID() )) echo $i; else echo $i+1; ?>"
+                                    class="<?php if($i==0 && !has_post_thumbnail( get_the_ID() ))echo'active'; ?>"
+                                    style="background: url('<?php echo $sliderCat[$nameImg]['url'] ?>')">
+                                </li>
+
+                                <?php endif; ?>
+
+                                <?php endfor; ?>
+                            </ol>
+                            <div class="carousel-inner">
+                                <?php if (has_post_thumbnail( get_the_ID() ) ): ?>
+                                <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'single-post-thumbnail' ); ?>
+                                <div class="carousel-item active">
+                                    <img src="<?php echo $image[0]; ?>"
+                                        alt="<?php custom_the_post_thumbnail_caption(); ?>">
+                                </div>
+                                <?php endif; $i = 0;?>
+                                <?php foreach($sliderCat as $ks=>$vs) : ?>
+                                <?php if($sliderCat[$ks] != false) : ?>
+                                <div
+                                    class="carousel-item <?php if($ks == 'img_1' && ! has_post_thumbnail( get_the_ID() )) echo 'active'; ?>">
+                                    <img src="<?php echo $vs['url']; ?>" alt="<?php echo $vs['title']; ?>">
+                                </div>
+                                <?php $i++; endif; ?>
+                                <?php endforeach; ?>
+                            </div>
+                            <a class="carousel-control carousel-control-prev" href="#carouselImgSlideModal"
+                                role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control carousel-control-next" href="#carouselImgSlideModal"
+                                role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                        <div class="quote-orverlay"></div>
+                    </div>
+
+                    <?php else :?>
+
+                    <?php if (has_post_thumbnail( get_the_ID() ) ): ?>
+                    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'single-post-thumbnail' ); ?>
+                    <img src="<?php echo $image[0]; ?>" alt="<?php custom_the_post_thumbnail_caption(); ?>">
+                    <?php endif;?>
+
+                    <?php endif; ?>
+                </div>
+                <!-- end slide imgage -->
+
+                <?php $basicInformation = get_field('basic_information', get_the_ID()); ?>
+                <?php if($basicInformation != NULL && $basicInformation['location'] != '') :?>
+                <div class="sprow-utilities sm-basic row">
+                    <div class="sprow-title col-md-4">
+                        <h4><?php _e('基本情報', 'hotel-center-lite-child'); ?></h4>
+                    </div>
+                    <div class="sprow-basic-utilities col-md-8">
+                        <div class="row">
+                            <?php if($basicInformation['location'] != ''){ ?>
+                            <div class="sprow-item col-md-6">
+                                <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/services/is.png"
+                                    alt="">
+                                <div class="sprow-text"><?php echo $strLocation; ?></div>
+                                <div class="sprow-value"><?php echo $basicInformation['location']; ?>
+                                </div>
+                            </div>
+                            <?php } ?>
+                            <?php if($basicInformation['opening_hours'] != ''){ ?>
+                            <div class="sprow-item col-md-6">
+                                <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/facilities/u-f3.png"
+                                    alt="">
+                                <div class="sprow-text"><?php echo $strOpeningHours; ?></div>
+                                <div class="sprow-value"><?php echo $basicInformation['opening_hours']; ?>
+                                </div>
+                            </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+    <!-- other service -->
+    <div class="container">
+        <section class="single-service">
+            <div class="single-service-wrap">
+                <h2 class="sw-title cl-title text-center">
+                    <span class="cl-main-title change-cl"><?php _e('他のサービス', 'hotel-center-lite-child') ?></span>
+                    <span class="cl-sub-title"><?php _e('他のサービス', 'hotel-center-lite-child') ?></span>
+                </h2>
+                <div class="service-carousel">
+                    <div class="container-fluid">
+                        <div id="multi-carousel-service" class="service-carousel">
+                            <div class="owl-carousel owl-theme">
+                                <?php 
+                                $args = array(
+                                    'post_type'     =>      'service',
+                                    'orderby'       =>      'date',
+                                    'order'         =>      'DESC',
+                                    'post_status'   =>      'publish',
+                                    'posts_per_page'=>      12
+                                );
+                                $query = new WP_Query($args);
+                                $myPosts = $query->get_posts();
+                                // echo "<pre>";print_r($myPosts);exit;
+                                foreach($myPosts as $k=>$v) :
+                                ?>
+                                <div class="item">
+                                    <div class="panel panel-default">
+                                        <div class="panel-thumbnail">
+                                            <a href="/<?php echo $v->post_type . '/' .$v->post_name ?>.html"
+                                                title="<?php echo $v->post_title; ?>" class="thumb">
+
+                                                <div class="sw-img">
+
+                                                    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $v->ID ), 'single-post-thumbnail' ); ?>
+                                                    <img src="<?php if (has_post_thumbnail( $v->ID ) ){echo $image[0];} ?>"
+                                                        alt="<?php if (has_post_thumbnail( $v->ID ) ){custom_the_post_thumbnail_caption();}else{echo 'Not Image';} ?>">
+
+                                                    <div class="sw-orverlay"></div>
+                                                </div>
+
+                                                <div class="sw-content post-content">
+                                                    <div class="sw-cat text-center">
+                                                        <?php 
+                                                            $getCat = get_the_terms($v->ID,'service-category');
+                                                            foreach($getCat as $kCat=>$vCat){
+                                                                echo $vCat->name;
+                                                                break;
+                                                            }
+                                                        ?>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php endforeach; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+
+<?php 
+
+get_footer();
