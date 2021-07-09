a111d85499814fbf68fd221e5b41814301d6b5ec
Phuc Nguyen <hoangphuc2587@gmail.com>
2021-07-09 17:55:46 +0700

    update about us , facilities page

--- wp-content/themes/hotel-center-lite-child/page-facility.php
+++ wp-content/themes/hotel-center-lite-child/page-facility.php
@@ -18,8 +18,8 @@ $showAboutPage = get_field('show_about_page');
     <div class="sr-title">
         <div class="sr-title-wrap container">
             <h2 class="cl-title text-center">
-                <span class="cl-main-title change-cl"><?php echo _e('特色','hotel-center-lite-child') ?></span>
-                <span class="cl-sub-title"><?php echo _e('特色','hotel-center-lite-child') ?></span>
+                <span class="cl-main-title change-cl"><?php echo _e('FACILITIES','hotel-center-lite-child') ?></span>
+                <span class="cl-sub-title"><?php echo _e('施設','hotel-center-lite-child') ?></span>
             </h2>
             <div class="cl-tax-share">
                 <a href="#">
@@ -53,34 +53,28 @@ $showAboutPage = get_field('show_about_page');
             // echo "<pre>";print_r($my_posts);exit;         
             foreach($my_posts as $k=>$v) : ?>
             <div class="col-lg-4 col-md-6 col-sm-12">
-                <div class="pfacilities-item">
-                    <div class="pfacilities-img">
+                <div class="card">                    
                         <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $v->ID ), 'single-post-thumbnail' ); ?>
-                        <img src="<?php if (has_post_thumbnail( $v->ID ) ){echo $image[0];} ?>" alt="<?php if (has_post_thumbnail( $v->ID ) ){custom_the_post_thumbnail_caption();}else{echo 'Not Image';} ?>">
-
+                        <a class="top-image" href="<?php echo home_url($v->post_type . '/' .$v->post_name .'.html')?>">
+                        <img class="card-img-top" src="<?php if (has_post_thumbnail( $v->ID ) ){echo $image[0];} ?>" alt="<?php if (has_post_thumbnail( $v->ID ) ){custom_the_post_thumbnail_caption();}else{echo 'Not Image';} ?>"></a>
                         <?php 
-                            $getCat = get_the_terms($v->ID,'facilities');
-                            // var_dump($getCat);
+                            $getCat = get_the_terms($v->ID,'facilities');                            
                             foreach($getCat as $kCat=>$vCat){
-                                ?>
+                        ?>
                                 <div class="pfnote">
                                 <a href="/<?php echo $vCat->taxonomy; ?>/<?php echo $vCat->slug; ?>.html" target="_blank" rel="noopener noreferrer"><?php echo $vCat->name; ?></a>
                                 </div>
-                                <?php
+                        <?php
                                 break;
                             }
                         ?>
-                    </div>
-                    <div class="pfacilities-body">
-                        <div class="pfcontent">
-                            <h3 class="pftitle"><?php echo $v->post_title; ?></h3>
-                            <p class="pfexcerpt"><?php echo $v->post_excerpt; ?></p >
+                        <div class="card-body">
+                           <a href="<?php echo home_url($v->post_type . '/' .$v->post_name .'.html')?>">
+                               <h5 class="card-title pftitle" title="<?php echo $v->post_title; ?>"><?php echo $v->post_title; ?></h5>
+                            </a>   
+                           <p class="card-text pfexcerpt"><?php echo $v->post_excerpt; ?></p>
                         </div>
-                        <div class="pffooter btn-direct">
-                            <a href="/<?php echo $v->post_type . '/' .$v->post_name ?>.html"
-                                rel="noopener noreferrer"><?php _e('もっと見る','hotel-center-lite-child') ?></a>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
             <?php endforeach; ?>
