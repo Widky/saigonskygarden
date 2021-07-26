3dacb135b12cbfd3283e9e562e43076f5956d843
Widky Nguyen <sba.nttri@gmail.com>
2021-07-26 18:24:44 +0700

    - Fix facilities in event and height attraction

--- wp-content/themes/hotel-center-lite-child/single-event.php
+++ wp-content/themes/hotel-center-lite-child/single-event.php
@@ -41,29 +41,29 @@ $event_cats = get_terms( array(
 
 ?>
 <style type="text/css">
-.carousel-facilities-about .owl-carousel .fiw-img img{
+.carousel-facilities-about .owl-carousel .fiw-img img {
     height: 534px;
 }
 
-.carousel-facilities-about .owl-carousel .owl-item{
+.carousel-facilities-about .owl-carousel .owl-item {
     opacity: 0.5;
 }
 
-.carousel-facilities-about .owl-carousel .active{
+.carousel-facilities-about .owl-carousel .active {
     opacity: 1 !important;
 }
 
-.container-facilities-about{
+.container-facilities-about {
     padding-left: 0px;
     padding-right: 0px
 }
 
-.container-facilities-about .wrap-facilities-about{
-   width: 100%;
-   position: relative;
+.container-facilities-about .wrap-facilities-about {
+    width: 100%;
+    position: relative;
 }
 
-.container-facilities-about .wrap-facilities-about .mask-facilities-about{
+.container-facilities-about .wrap-facilities-about .mask-facilities-about {
     width: 100%;
     height: 930px;
     position: absolute;
@@ -71,14 +71,14 @@ $event_cats = get_terms( array(
     padding: 0px 60px;
 }
 
-.container-facilities-about .wrap-facilities-about .mask-facilities-about .content-facilities-about{
+.container-facilities-about .wrap-facilities-about .mask-facilities-about .content-facilities-about {
     background: #F6F5F1;
     position: relative;
     height: 100%;
 }
 
 
-.owl-carousel .owl-nav button.owl-prev{
+.owl-carousel .owl-nav button.owl-prev {
     position: absolute;
     top: 45%;
     left: 195px;
@@ -88,7 +88,7 @@ $event_cats = get_terms( array(
     opacity: 0.5;
 }
 
-.owl-carousel .owl-nav button.owl-next{
+.owl-carousel .owl-nav button.owl-next {
     position: absolute;
     top: 45%;
     right: 195px;
@@ -98,17 +98,20 @@ $event_cats = get_terms( array(
     opacity: 0.5;
 }
 
-.owl-carousel .owl-nav button.owl-prev:focus, .owl-carousel .owl-nav button.owl-next:focus{
+.owl-carousel .owl-nav button.owl-prev:focus,
+.owl-carousel .owl-nav button.owl-next:focus {
     outline: none;
 }
 
-.owl-carousel .owl-nav button.owl-prev:hover, .owl-carousel .owl-nav button.owl-next:hover{
-   background: #869791 !important;
+.owl-carousel .owl-nav button.owl-prev:hover,
+.owl-carousel .owl-nav button.owl-next:hover {
+    background: #869791 !important;
 }
 
 
 
-.owl-carousel .owl-nav button.owl-next span, .owl-carousel .owl-nav button.owl-prev span {
+.owl-carousel .owl-nav button.owl-next span,
+.owl-carousel .owl-nav button.owl-prev span {
     background: none;
     border: solid #FFFFFF;
     border-width: 0 2px 2px 0;
@@ -118,15 +121,15 @@ $event_cats = get_terms( array(
 }
 
 .owl-carousel .owl-nav button.owl-next span {
-    -webkit-transform: rotate( -45deg);
-    -ms-transform: rotate( -45deg);
-    transform: rotate( -45deg);
+    -webkit-transform: rotate(-45deg);
+    -ms-transform: rotate(-45deg);
+    transform: rotate(-45deg);
     margin-left: 15px;
 }
 
 .owl-carousel .owl-nav button.owl-prev span {
     -webkit-transform: rotate(135deg);
-    -ms-transform: rotate( 135deg);
+    -ms-transform: rotate(135deg);
     transform: rotate(135deg);
     margin-left: 25px;
 }
@@ -135,45 +138,48 @@ $event_cats = get_terms( array(
 
 @media screen and (max-width: 767.98px) {
 
-    .carousel-facilities-about .owl-carousel .fiw-img img{
-      height: 174px;
+    .carousel-facilities-about .owl-carousel .fiw-img img {
+        height: 174px;
     }
 
-    .container-facilities-about .wrap-facilities-about .mask-facilities-about{
+    .container-facilities-about .wrap-facilities-about .mask-facilities-about {
         width: 100%;
         height: 450px;
         position: absolute;
         z-index: -1;
         padding: 0px 20px;
     }
-   
+
 }
 
 
 /* size E SM - < 599px */
 
 @media screen and (max-width: 599px) {
-    .owl-carousel .owl-nav button.owl-next{
+    .owl-carousel .owl-nav button.owl-next {
         right: 55px;
         top: 35%;
         height: 35px;
         width: 35px;
     }
-    .owl-carousel .owl-nav button.owl-prev span{
+
+    .owl-carousel .owl-nav button.owl-prev span {
         margin-left: 13px;
     }
-    .owl-carousel .owl-nav button.owl-prev{
+
+    .owl-carousel .owl-nav button.owl-prev {
         left: 55px;
         top: 35%;
         height: 35px;
         width: 35px;
     }
-    .owl-carousel .owl-nav button.owl-next span{
+
+    .owl-carousel .owl-nav button.owl-next span {
         margin-left: 4px;
     }
 
-    .carousel-facilities-about .owl-carousel .fiw-img img{
-      height: 133px;
+    .carousel-facilities-about .owl-carousel .fiw-img img {
+        height: 133px;
     }
 }
 </style>
@@ -182,24 +188,24 @@ $event_cats = get_terms( array(
     <div class="row single-event">
         <div class="container">
             <section class="single-event-sec">
-            <div class="sr-title">
-                <div class="sr-title-wrap container">
-                    <h2 class="cl-title text-center">
-                        <span class="cl-main-title change-cl"><?php the_title(); ?></span>
-                        <span class="cl-sub-title"><?php echo $sub_title; ?></span>
-                    </h2>
-                    <div class="cl-tax-share">
-                        <a href="#">
-                            <i class="fas fa-share-alt"></i>
-                            <span><?php echo $strButtonShare; ?></span>
-                        </a>
+                <div class="sr-title">
+                    <div class="sr-title-wrap container">
+                        <h2 class="cl-title text-center">
+                            <span class="cl-main-title change-cl"><?php the_title(); ?></span>
+                            <span class="cl-sub-title"><?php echo $sub_title; ?></span>
+                        </h2>
+                        <div class="cl-tax-share">
+                            <a href="#">
+                                <i class="fas fa-share-alt"></i>
+                                <span><?php echo $strButtonShare; ?></span>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
                 <!-- <div class="page_title text-center my-5"> -->
-                    <h1><?php //the_title(); ?></h1>
-                    <p><?php //echo $sub_title; ?></p>
+                <h1><?php //the_title(); ?></h1>
+                <p><?php //echo $sub_title; ?></p>
                 <!-- </div> -->
                 <div class="day-of-event mb-2">
                     <?php if(!empty($day_of_event)) {
@@ -264,7 +270,7 @@ $event_cats = get_terms( array(
                     $related_items = new WP_Query( $args );
                     // loop over query
                     if ($related_items->have_posts()) :
-                        ?>   
+                        ?>
                     <?php while ( $related_items->have_posts() ) : $related_items->the_post();
 
                         $sub_arr = array(
@@ -272,26 +278,27 @@ $event_cats = get_terms( array(
                             //'fields' => array('names')
                         );  
                         $sub_cat = wp_get_post_terms(get_the_ID(),'event-category',$sub_arr) ;
-                        ?> 
-                            <div class="event_item">
-                                    <?php if(!empty( $sub_cat)) {
+                        ?>
+                    <div class="event_item">
+                        <?php if(!empty( $sub_cat)) {
                                         $sub_cat_color = get_field('category_background_color',$sub_cat[0]->taxonomy.'_'.$sub_cat[0]->term_id) ;
                                         $sub_cat_color = !empty($sub_cat_color) ? $sub_cat_color : '#166772';
                                         ?>
-                                        <div class="sub_cat" style="background: <?php echo $sub_cat_color; ?>"><?php echo $sub_cat[0]->name ;?></div>
-                                    <?php } ?>
+                        <div class="sub_cat" style="background: <?php echo $sub_cat_color; ?>">
+                            <?php echo $sub_cat[0]->name ;?></div>
+                        <?php } ?>
 
-                                <div class="event_img">
-                                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium') ?></a>
-                                </div>
+                        <div class="event_img">
+                            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium') ?></a>
+                        </div>
 
-                                <div class="event_info p-3">
-                                    <h3 class="mb-2 text-center">
-                                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                    </h3>
-                                    <div class="event_excerpt">
-                                        <p>
-                                            <?php
+                        <div class="event_info p-3">
+                            <h3 class="mb-2 text-center">
+                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                            </h3>
+                            <div class="event_excerpt">
+                                <p>
+                                    <?php
                                                 $event_len = mb_strlen(get_the_excerpt(),'UTF-8');
                                                 if($locale == 'ja'){
                                                     echo mb_substr(get_the_excerpt(), 0, 25,'UTF-8');
@@ -305,19 +312,19 @@ $event_cats = get_terms( array(
                                                         }    
                                                 } 
                                             ?>
-                                        </p>
-                                    </div>
-                                </div>
+                                </p>
                             </div>
-                        <?php 
+                        </div>
+                    </div>
+                    <?php 
                             endwhile;
                             // echo '</ul>';
                             endif;
                             // Reset Post Data
                             wp_reset_postdata();
                             ?>
-                    </div>
-               
+                </div>
+
             </section>
         </div>
     </div>
@@ -331,7 +338,8 @@ $event_cats = get_terms( array(
                 <div class="sr-title">
                     <div class="sr-title-wrap container">
                         <h2 class="cl-title text-center">
-                            <span class="cl-main-title change-cl"><?php echo _e('FACILITIES','hotel-center-lite-child') ?></span>
+                            <span
+                                class="cl-main-title change-cl"><?php echo _e('FACILITIES','hotel-center-lite-child') ?></span>
                             <span class="cl-sub-title"><?php echo _e('施設','hotel-center-lite-child') ?></span>
                         </h2>
                     </div>
@@ -339,7 +347,7 @@ $event_cats = get_terms( array(
                 <div class="carousel-facilities-about">
                     <div class="carousel-facilities-about-wrap">
                         <div class="owl-carousel owl-theme">
-                                <?php 
+                            <?php 
                             $args = array(
                                 'post_type'     =>      'facilities',
                                 'orderby'       =>      'date',
@@ -351,20 +359,26 @@ $event_cats = get_terms( array(
                             $i = 0;
                             // echo "<pre>";print_r($myPosts);
                             foreach($myPosts as $k=>$v) :
-                                if (has_post_thumbnail( $v->ID ) ): ?>
-                                <div class="item <?php if($i == 0) echo 'active'; ?>">
-                                    <a href="<?php echo home_url($v->post_type . '/' .$v->post_name .'.html')?>">
-                                        <div class="fiw-img">
-                                            <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $v->ID ), 'single-post-thumbnail' ); ?>
-                                            <img src="<?php echo $image[0]; ?>"
-                                                alt="<?php custom_the_post_thumbnail_caption(); ?>">
-                                        </div>
-                                    </a>
-                                </div>
-                                <?php $i++; ?>
-                                <?php endif; ?>
-                                <?php endforeach; ?>
-                        </div>                    
+                                $image = get_field('image_show_in_about_page',$v->ID);
+
+                            if ($image != NULL ): ?>
+                            <div class="item <?php if($i == 0) echo 'active'; ?>">
+                                <a href="<?php echo home_url($v->post_type . '/' .$v->post_name .'.html')?>">
+                                    <div class="fiw-img">
+                                        <?php 
+                                        if($image != NULL){
+                                            echo '<img src="'.$image['url'].'" alt="'.$image['filename'].'">';
+                                        }else{
+                                            echo '<img src="" alt="'._('Not Image').'">';
+                                        }
+                                        ?>
+                                    </div>
+                                </a>
+                            </div>
+                            <?php $i++; ?>
+                            <?php endif; ?>
+                            <?php endforeach; ?>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -372,7 +386,7 @@ $event_cats = get_terms( array(
     </div>
 
     <?php //get_sidebar();?>
-    
+
     <div class="clear"></div>
 </div><!-- container -->
 
