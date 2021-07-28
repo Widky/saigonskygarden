4ca60baf64a8c096edb13e08e33f3d268f5286c3
Widky Nguyen <sba.nttri@gmail.com>
2021-07-28 11:49:58 +0700

    - Add telewwork and block video in homepage

--- wp-content/themes/hotel-center-lite-child/template-parts/block/block-features.php
+++ wp-content/themes/hotel-center-lite-child/template-parts/block/block-features.php
@@ -1,6 +1,38 @@
 <?php $the_cat = 'post-home'; ?>
-<div class="row position-relative apartment_frame">
+<div class="row position-relative apartment_frame telewwork-mg">
     <div class="apratment_bg"></div>
+    <?php
+    $args = array(
+        'post_type'         =>  'post',
+        'orderby'           =>  'date',
+        'order'             =>  'DESC',
+        'post_status'       =>  'publish',
+        'posts_per_page'    =>  1,
+        'pagename'          => 'telewwork',
+    );
+    $query = new WP_Query($args);
+    $my_posts = $query->get_posts();
+    // var_dump($my_posts);exit;
+    if($my_posts) : ?>
+    <div class="col-12 apartment_block p-0 ">
+        <div class="fw container">
+            <div class="row fw-items">
+                <div class="telewwork">
+                    <div class="tw-img">
+                        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $my_posts[0]->ID ), 'single-post-thumbnail' ); ?>
+                        <img src="<?php if (has_post_thumbnail( $my_posts[0]->ID ) ){echo $image[0];} ?>"
+                            alt="<?php if (has_post_thumbnail( $my_posts[0]->ID ) ){custom_the_post_thumbnail_caption();}else{echo 'Not Image';} ?>">
+                    </div>
+                    <div class="tw-content">
+                        <?php echo $my_posts[0]->post_content; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <?php endif; ?>
+
     <div class="col-12 apartment_block p-0">
         <h2 class="cl-title text-center">
             <span class="cl-main-title"><?php _e('Feature','hotel-center-lite-child') ?></span>
@@ -35,30 +67,30 @@
                     ?>
                 <div class="fw-items-wrap col-md-4 col-sm-12 col-12">
                     <!-- <a href="<?php //echo home_url( $v->post_name .'.html' ); ?>"> -->
-                        <div class="fw-img <?php if($i == 3) echo 'fw-set-orverlay'; ?>">
+                    <div class="fw-img <?php if($i == 3) echo 'fw-set-orverlay'; ?>">
 
-                            <?php 
+                        <?php 
                                 $showHome = get_field('show_home', $v->ID);
                                 // echo "<pre>";var_dump($showHome);
                                 if($showHome && $showHome != NULL){ ?>
 
-                            <img src="<?php echo $showHome['image_for_post_page']['url']; ?>"
-                                alt="<?php custom_the_post_thumbnail_caption(); ?>">
+                        <img src="<?php echo $showHome['image_for_post_page']['url']; ?>"
+                            alt="<?php custom_the_post_thumbnail_caption(); ?>">
 
-                            <?php 
+                        <?php 
                                 $overlayImage = $showHome['overlay_image'];
                                 if($i == 3 && $overlayImage ) {$overlayImage = $overlayImage['url']; echo "<div class='fw-img-orverlay' style='background: url(".$overlayImage.")'></div>";}
                                 ?>
-                            <?php } ?>
+                        <?php } ?>
+                    </div>
+                    <div class="fw-content post-content">
+                        <div class="fw-content-wrap">
+                            <h3 class="post-title">
+                                <?php echo "<span class='set-pos'><span class='fw-title-number'><span>".$i."</span></span></span>"; echo '<span class="fw-title-content">'.$v->post_title.'<span>' ?>
+                            </h3>
+                            <pre class="post-excerpt"><?php echo $v->post_excerpt; ?></pre>
                         </div>
-                        <div class="fw-content post-content">
-                            <div class="fw-content-wrap">
-                                <h3 class="post-title">
-                                    <?php echo "<span class='set-pos'><span class='fw-title-number'><span>".$i."</span></span></span>"; echo '<span class="fw-title-content">'.$v->post_title.'<span>' ?>
-                                </h3>
-                                <pre class="post-excerpt"><?php echo $v->post_excerpt; ?></pre>
-                            </div>
-                        </div>
+                    </div>
 
                     <!-- </a> -->
                 </div>
@@ -66,7 +98,8 @@
             </div>
         </div>
         <div class="fw-btn-direct btn-direct">
-            <a href="<?php echo home_url('feature.html'); ?>" rel="noopener noreferrer"><?php echo __('View More','hotel-center-lite-child') ?></a>
+            <a href="<?php echo home_url('feature.html'); ?>"
+                rel="noopener noreferrer"><?php echo __('View More','hotel-center-lite-child') ?></a>
         </div>
     </div>
 </div>
--- wp-content/themes/hotel-center-lite-child/template-parts/block/block-video.php
+++ wp-content/themes/hotel-center-lite-child/template-parts/block/block-video.php
@@ -9,22 +9,68 @@ $args = array(
 );
 $query = new WP_Query($args);
 $my_posts = $query->get_posts();
+// var_dump($my_posts);exit;
 if($my_posts) :
     $showAboutPage = get_field('show_about_page', $my_posts[0]->ID);
 ?>
-<div class="row position-relative">
-    <div class="col-12 p-0">
-    <div class="home-video">
-            <div class="about-video">
-                <?php if($showAboutPage['video_about']['url']) : ?>
-                    <iframe width="560" height="315"
-                        src="<?php echo $showAboutPage['video_about']['url']; ?>&showinfo=0&rel=0" frameborder="0"
-                        allowfullscreen="allowfullscreen"></iframe>
-                <?php endif; ?>
+
+<div class="popular-video">
+    <div class="container">
+        <h2 class="cl-title text-center">
+            <span class="cl-main-title change-cl"><?php echo _e('POPULAR VIDEO','hotel-center-lite-child') ?></span>
+            <span class="cl-sub-title"><?php echo _e('紹介動画','hotel-center-lite-child') ?></span>
+        </h2>
+        <div class="content-popular-video">
+            <div class="row rvideo position-relative">
+                <div class="col-lg-6 col-md-12 col-12">
+                    <div class="home-video">
+                        <div class="card about-video">
+                            <?php if($showAboutPage['video_about']['url']) : ?>
+                            <div class="card-img-top">
+                                <iframe width="560" height="315"
+                                    src="<?php echo $showAboutPage['video_about']['url']; ?>&showinfo=0&rel=0"
+                                    frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+                            </div>
+                            <?php endif; ?>
+                            <div class="card-body">
+                                <?php if(get_bloginfo('language') == 'ja'){?>
+                                <div class="card-day"><?php echo date('Y年n月j日', strtotime($my_posts[0]->post_date)); ?></div>
+                                <?php }else{ ?>
+                                    <div class="card-day"><?php echo date('F j, Y', strtotime($my_posts[0]->post_date)); ?></div>
+                                <?php } ?>
+                                <h5 class="card-title"><?php echo $showAboutPage['video_about']['video_name']; ?></h5>
+                                <p class="card-text"><?php echo $showAboutPage['video_about']['video_description']; ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 change-mgt">
+                    <div class="home-video">
+                        <div class="card about-video">
+                            <?php if($showAboutPage['video_about_2']['url']) : ?>
+                            <div class="card-img-top">
+                                <iframe width="560" height="315"
+                                    src="<?php echo $showAboutPage['video_about_2']['url']; ?>&showinfo=0&rel=0"
+                                    frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+                            </div>
+                            <?php endif; ?>
+                            <div class="card-body">
+                            <?php if(get_bloginfo('language') == 'ja'){?>
+                                <div class="card-day"><?php echo date('Y年n月j日', strtotime($my_posts[0]->post_date)); ?></div>
+                                <?php }else{ ?>
+                                    <div class="card-day"><?php echo date('F j, Y', strtotime($my_posts[0]->post_date)); ?></div>
+                                <?php } ?>
+                                <h5 class="card-title"><?php echo $showAboutPage['video_about_2']['video_name']; ?></h5>
+                                <p class="card-text"><?php echo $showAboutPage['video_about_2']['video_description']; ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
 
 <?php endif; ?>
